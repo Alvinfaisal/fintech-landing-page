8bf4f940c364115041dee7b36a9628b46ad649ff
Faisal <faisalvin15@gmail.com>
2024-08-03 21:44:48 +0700

    add navbar collapse

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@
 const navbar = document.querySelector(".navbar");
 const btnOpenNav = document.querySelector("#btnOpenNav");
 const btnCloseNav = document.querySelector("#btnCloseNav");
+const bodyTag = document.querySelector("body");
 
 btnOpenNav.addEventListener("click", () => {
   navbar.classList.add("openNav");
@@ -9,6 +10,7 @@ btnOpenNav.addEventListener("click", () => {
   if (navbar.classList.contains("openNav")) {
     btnOpenNav.classList.add("hide");
     btnCloseNav.classList.remove("hide");
+    bodyTag.classList.add("scroll-hidden");
   }
 });
 
@@ -16,4 +18,18 @@ btnCloseNav.addEventListener("click", () => {
   navbar.classList.remove("openNav");
   btnCloseNav.classList.add("hide");
   btnOpenNav.classList.remove("hide");
+  bodyTag.classList.remove("scroll-hidden");
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    !e.target.classList.contains("navbar") &&
+    e.target.id !== "btnOpenNav" &&
+    !e.target.classList.contains("navbar-link")
+  ) {
+    navbar.classList.remove("openNav");
+    btnCloseNav.classList.add("hide");
+    btnOpenNav.classList.remove("hide");
+    bodyTag.classList.remove("scroll-hidden");
+  }
 });
